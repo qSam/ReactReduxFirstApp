db2ce221fcb610874b1c717f558872e2b421feda
Qasim Laghari <go.jets.laghari@gmail.com>
2016-03-27 22:30:59 -0500

    Commit initial structure for Youtube API React App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyD3o7QSIZ4qAYJ_LCDnlu8ymyEE0QQLHh8';
+
 // Create a new component that produes HTML
+const App = () => {
+  return (
+         <div>
+            <SearchBar />
+         </div>
+         );
+}
 
 
-
-// Insert component HTML into DOM 
+// Insert insert of App into Container DOM element
+ReactDOM.render(<App />, document.querySelector('.container'));
