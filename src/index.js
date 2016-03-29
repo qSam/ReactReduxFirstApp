c9c21d027f9f690cdfe3cf34a43edbe7eec400a4
Qasim Laghari <go.jets.laghari@gmail.com>
2016-03-28 20:15:52 -0500

    Updating React code with Youtube API call rendering

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail.js';
+
 
 const API_KEY = 'AIzaSyD3o7QSIZ4qAYJ_LCDnlu8ymyEE0QQLHh8';
 
+
 // Create a new component that produes HTML
-const App = () => {
-  return (
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+             videos:videos,
+             selectedVideo: videos[0]
+            });
+    });
+  }
+
+  render() {
+        return (
          <div>
             <SearchBar />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos={this.state.videos}/>
          </div>
          );
+       }
 }
 
 
